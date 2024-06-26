78118ba7c3aadab7f084ba07a73fff4866e2c393
Nazar <nazar.tsap2349@gmail.com>
2024-06-26 15:53:56 +0300

    new_file

--- Scrumdinger/Models/CardView.swift
+++ Scrumdinger/Models/CardView.swift
@@ -8,13 +8,20 @@ struct CardView: View {
             VStack(alignment: .leading){
                 Text(scrum.title)
                     .font(.headline)
+                    .accessibilityAddTraits(.isHeader)
                 Spacer()
                 HStack{
                     Label("\(scrum.attendees.count)", systemImage: "person.3")
+                        .accessibilityLabel("\(scrum.attendees.count) attendees")
                     Spacer()
-                    Label("\(scrum.lengthInMinutes)", systemImage: "circle")
+                    Label("\(scrum.lengthInMinutes)", systemImage: "clock")
+                        .accessibilityLabel("\(scrum.lengthInMinutes) minute meeting")
+                        .labelStyle(.trailingIcon)
                 }
+                .font(.caption)
             }
+            .padding()
+            .foregroundColor(scrum.theme.accentColor)
         }
     }
     struct CardView_Previews: PreviewProvider {
